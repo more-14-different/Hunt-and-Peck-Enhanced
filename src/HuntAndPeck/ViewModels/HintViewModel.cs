e6d0186c5920cbcaba5f762f2fc26885559b1e84
more_14_different <more.14.different@gmail.com>
2026-07-11 00:51:40 +0800

    Apply appearance settings to hint overlay

--- src/HuntAndPeck/ViewModels/HintViewModel.cs
+++ src/HuntAndPeck/ViewModels/HintViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using HuntAndPeck.Models;
 using HuntAndPeck.Properties;
 
+using System.Windows.Media;
+
 namespace HuntAndPeck.ViewModels
 {
     public class HintViewModel : NotifyPropertyChanged
@@ -12,6 +14,9 @@ namespace HuntAndPeck.ViewModels
         private string _backgroundColorReadValue;
         private string _inactiveBackgroundColorReadValue;
         private string _textColorReadValue;
+        private Brush _backgroundBrush;
+        private Brush _inactiveBackgroundBrush;
+        private Brush _textBrush;
 
         public HintViewModel(Hint hint)
         {
@@ -21,6 +26,9 @@ namespace HuntAndPeck.ViewModels
             BackgroundColorReadValue = Settings.Default.BackgroundColor;
             InactiveBackgroundColorReadValue = Settings.Default.InactiveBackgroundColor;
             TextColorReadValue = Settings.Default.TextColor;
+            BackgroundBrush = CreateBrush(BackgroundColorReadValue);
+            InactiveBackgroundBrush = CreateBrush(InactiveBackgroundColorReadValue);
+            TextBrush = CreateBrush(TextColorReadValue);
         }
 
         public Hint Hint { get; set; }
@@ -28,7 +36,12 @@ namespace HuntAndPeck.ViewModels
         public bool Active
         {
             get { return _active; }
-            set { _active = value; NotifyOfPropertyChange(); }
+            set
+            {
+                _active = value;
+                NotifyOfPropertyChange();
+                NotifyOfPropertyChange("CurrentBackgroundBrush");
+            }
         }
 
         public string Label
@@ -65,6 +78,43 @@ namespace HuntAndPeck.ViewModels
         {
             get { return _textColorReadValue; }
             set { _textColorReadValue = value; NotifyOfPropertyChange(); }
+        }
+
+        public Brush BackgroundBrush
+        {
+            get { return _backgroundBrush; }
+            set { _backgroundBrush = value; NotifyOfPropertyChange(); NotifyOfPropertyChange("CurrentBackgroundBrush"); }
+        }
+
+        public Brush InactiveBackgroundBrush
+        {
+            get { return _inactiveBackgroundBrush; }
+            set { _inactiveBackgroundBrush = value; NotifyOfPropertyChange(); NotifyOfPropertyChange("CurrentBackgroundBrush"); }
+        }
+
+        public Brush CurrentBackgroundBrush
+        {
+            get { return Active ? BackgroundBrush : InactiveBackgroundBrush; }
+        }
+
+        public Brush TextBrush
+        {
+            get { return _textBrush; }
+            set { _textBrush = value; NotifyOfPropertyChange(); }
+        }
+
+        private static Brush CreateBrush(string color)
+        {
+            try
+            {
+                var brush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(color));
+                brush.Freeze();
+                return brush;
+            }
+            catch
+            {
+                return Brushes.Black;
+            }
         }
     }
 }
